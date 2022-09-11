3516f18511aec5c0b40acbb65ba48e5380c9ca80
K-S-P-B-VARMA <113291629+K-S-P-B-VARMA@users.noreply.github.com>
2022-09-11 17:56:57 +0530

    Python Code for prime numbers

---
+++ prime number.py
@@ -0,0 +1,15 @@
+num=int(input("enter a number"))
+if num>1:
+    
+    for i in range(2,num):
+        
+        if num%i ==0:
+            
+            print("not a prime number")
+        
+        else:
+            
+            print("it is a prime number")
+            break
+else:
+    print("not a prime number")
